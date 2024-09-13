3a05a9487aab77bc0d0a6036a1f033f14822ae60
rosencrantz96 <topgdvidsyb@naver.com>
2024-09-13 13:53:17 +0900

    Fix: signup bug updated

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // app.js
 function login(username, password) {
- 	// 잘못된 조건처리로인해로그인오류발생
-	if (username && password) {
+ 	// 수정: username과 password가 모두 있는 경우에만 로그인성공
+	if (username !== "" && password !== "") {
  		console.log("Login successful");
  	} else {
  		console.log("Login failed");
  	}
  }
-// 버그: username
