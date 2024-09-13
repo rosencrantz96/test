3122ba1823cfd0ee8feab714e66541dadbff5bfb
rosencrantz96 <topgdvidsyb@naver.com>
2024-09-13 14:04:29 +0900

    Feat: new login feature

---
+++ new_login.js
@@ -0,0 +1,17 @@
+// new_login.js
+function newLogin(username, password) {
+	if (username && password) {
+        	return "Login successful";
+    	} else {
+        	return "Login failed";
+    	}
+}
+
+// 추가기능: 비밀번호보안확인
+function validatePassword(password) {
+    	if (password.length < 8) {
+        	return "Password too short";
+    	} else {
+        	return "Password is strong enough";
+    	}
+}
